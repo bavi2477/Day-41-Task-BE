7fd46af25100517c6397817b3bcfe8c1df7648ea
bavi2477 <bavithra.sjh@gmail.com>
2024-03-16 13:35:35 +0530

    mail url

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -9,7 +9,7 @@ router.post('/forgot-password', requestPasswordReset)
 router.post('/reset-password/:token', resetPassword)
 router.get('/reset-password/:token', (req, res) => {
     const { token } = req.params;
-    res.redirect(`http://localhost:5173/reset-password/${token}`);
+    res.redirect(`https://splendorous-scone-1a65e6.netlify.app/reset-password/${token}`);
 });
 
 export default router;
