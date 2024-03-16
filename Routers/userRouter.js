aa6d0b4fa6679d7025b9e472595e1368594e2c20
bavi2477 <bavithra.sjh@gmail.com>
2024-03-16 14:05:20 +0530

    url

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -9,7 +9,7 @@ router.post('/forgot-password', requestPasswordReset)
 router.post('/reset-password/:token', resetPassword)
 router.get('/reset-password/:token', (req, res) => {
     const { token } = req.params;
-    res.redirect(`https://day-41-task-be.onrender.com/reset-password/${token}`);
+    res.redirect(`https://splendorous-scone-1a65e6.netlify.app/api/user/reset-password/${token}`);
 });
 
 export default router;
