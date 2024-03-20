fd2ffdcc1b351f974924426801b4acac7edf7190
bavi2477 <bavithra.sjh@gmail.com>
2024-03-20 18:35:59 +0530

    place change

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -6,11 +6,11 @@ const router = express.Router()
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.post('/forgot-password', requestPasswordReset)
-router.post('/reset-password/:token', resetPassword)
 router.get('/reset-password/:token', (req, res) => {
     const { token } = req.params;
     console.log(`Received GET request to redirect-reset-password with token: ${token}`);
     res.redirect(`http://localhost:5173/reset-password/${token}`);
 });
+router.post('/reset-password/:token', resetPassword)
 
 export default router;
